6b8e4b34d65f5618f1f2d0914935499eadb09063
Kristjan Lääne <xonproject@gmail.com>
2024-08-31 11:49:55 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Container from './components/Container';
+import Footer from './components/Footer';
+import HashtagList from './components/HashtagList';
+
+function App() {
+  return (
+    <>
+      <Footer />
+
+      <Container />
+
+      <HashtagList />
+    </>
+  );
+}
+
+export default App;
