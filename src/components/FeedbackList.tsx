618de13785be7db9cf5521cec6a53d43b987cdb7
Kristjan Lääne <xonproject@gmail.com>
2024-09-02 17:07:56 +0300

    fetch data

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,27 +1,25 @@
+import { useEffect, useState } from 'react';
 import FeedbackItem from './FeedbackItem';
 
-const feedbackItems = [
-  {
-    upvoteCount: 593,
-    badgeLetter: 'B',
-    companyName: 'ByteGrad',
-    text: 'test test test',
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 983,
-    badgeLetter: 'S',
-    companyName: 'Starbucks',
-    text: 'test test test test test test',
-    daysAgo: 12,
-  },
-];
-
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbackItems(data.feedbacks);
+      });
+  }, []);
+
   return (
     <ol className='feedback-list'>
       {feedbackItems.map((feedbackItem) => (
-        <FeedbackItem feedbackItem={feedbackItem} />
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
     </ol>
   );
