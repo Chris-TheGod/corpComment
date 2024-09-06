02b0797ef1fff4bf7c59444aa546243d5cc0dd63
Kristjan Lääne <xonproject@gmail.com>
2024-09-06 19:23:13 +0300

    custom hook for feedback items

--- src/contexts/FeedbackItemsContextProvider.tsx
+++ src/contexts/FeedbackItemsContextProvider.tsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { createContext, useMemo, useState } from 'react';
 import { TFeedbackItem } from '../lib/types';
+import { useFeedbackItems } from '../lib/hooks';
 
 type FeedbackItemsContextProviderProps = {
   children: React.ReactNode;
@@ -21,9 +22,8 @@ export const FeedbackItemsContext = createContext<TFeedbackItemsContext | null>(
 export default function FeedbackItemsContextProvider({
   children,
 }: FeedbackItemsContextProviderProps) {
-  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const { feedbackItems, isLoading, errorMessage, setFeedbackItems } =
+    useFeedbackItems();
   const [selectedCompany, setSelectedCompany] = useState('');
 
   const companyList = useMemo(
@@ -78,27 +78,6 @@ export default function FeedbackItemsContextProvider({
   const handleSelectCompany = (company: string) => {
     setSelectedCompany(company);
   };
-
-  useEffect(() => {
-    const fetchFeedbackItems = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetch(
-          'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
-        );
-        if (!res.ok) {
-          throw new Error();
-        }
-        const data = await res.json();
-        setFeedbackItems(data.feedbacks);
-      } catch (error) {
-        setErrorMessage('Something went wrong');
-      }
-      setIsLoading(false);
-    };
-
-    fetchFeedbackItems();
-  }, []);
 
   return (
     <FeedbackItemsContext.Provider
